b834e543bf974b20a8c08adb09006f324615a827
Pravin Prakash Kumar <pravinpk0707@gmail.com>
2024-05-22 00:56:04 -0700

    Actual Notification Data files

--- code/notificationbuilder/src/main/java/com/adobe/marketing/mobile/notificationbuilder/internal/util/IntentData.kt
+++ code/notificationbuilder/src/main/java/com/adobe/marketing/mobile/notificationbuilder/internal/util/IntentData.kt
@@ -1,4 +1,24 @@
+/*
+  Copyright 2024 Adobe. All rights reserved.
+  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under
+  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  OF ANY KIND, either express or implied. See the License for the specific language
+  governing permissions and limitations under the License.
+*/
+
 package com.adobe.marketing.mobile.notificationbuilder.internal.util
 
-class IntentData {
+import android.os.Bundle
+
+/**
+ * Class responsible for extracting notification data from an intent.
+ */
+class IntentData(private val extras: Bundle) : NotificationData {
+    override fun getString(key: String): String? = extras.getString(key)
+    override fun getInteger(key: String): Int? = extras.getString(key)?.toIntOrNull()
+    override fun getBoolean(key: String): Boolean? = extras.getString(key)?.toBoolean()
+    override fun getLong(key: String): Long? = extras.getString(key)?.toLongOrNull()
 }
