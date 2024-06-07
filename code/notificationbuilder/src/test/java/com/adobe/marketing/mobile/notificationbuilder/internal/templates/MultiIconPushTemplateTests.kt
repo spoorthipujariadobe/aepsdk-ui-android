d956e545968b720f75c2fa83bfb4bec9cd7a8852
Navratan Soni <navratans@adobe.com>
2024-06-08 01:05:21 +0530

    Spotless apply

--- code/notificationbuilder/src/test/java/com/adobe/marketing/mobile/notificationbuilder/internal/templates/MultiIconPushTemplateTests.kt
+++ code/notificationbuilder/src/test/java/com/adobe/marketing/mobile/notificationbuilder/internal/templates/MultiIconPushTemplateTests.kt
@@ -1,3 +1,14 @@
+/*
+  Copyright 2024 Adobe. All rights reserved.
+  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under
+  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  OF ANY KIND, either express or implied. See the License for the specific language
+  governing permissions and limitations under the License.
+*/
+
 package com.adobe.marketing.mobile.notificationbuilder.internal.templates
 
 import com.adobe.marketing.mobile.notificationbuilder.PushTemplateConstants
@@ -79,8 +90,10 @@ class MultiIconPushTemplateTests {
     @Test
     fun testMultiIconPushTemplateIncompleteJson() {
         val dataMap = getMockedDataMapWithForMultiIcon()
-        dataMap.replaceValueInMap(PushTemplateConstants.PushPayloadKeys.MULTI_ICON_ITEMS,
-            MOCK_MULTI_ICON_ITEM_PAYLOAD_INCOMPLETE_JSON)
+        dataMap.replaceValueInMap(
+            PushTemplateConstants.PushPayloadKeys.MULTI_ICON_ITEMS,
+            MOCK_MULTI_ICON_ITEM_PAYLOAD_INCOMPLETE_JSON
+        )
         val multiIconPushTemplate = MultiIconPushTemplate(MapData(dataMap))
         assertEquals(3, multiIconPushTemplate.templateItemList.size)
     }
